1e4eff222b2b7b1ff4bd4efe88227ca678404df4
Shawaal Nadeem <hafizshawalnadeem@gmail.com>
2023-10-26 13:31:03 +0500

    Class 2 completed

---
+++ Class 2/main.py
@@ -0,0 +1,13 @@
+name: str = "Shawaal Nadeem"
+fName: str = "Nadeem"
+education: str = "Bachlero's in Computer Sciences"
+age: int = 22
+card: str = f"""
+PIAIC Student Card
+Student Name: {name}
+Father's Name: {fName}
+Age: {age}
+Education: {education}
+"""
+# After = we place f
+print(card)
---
+++ Class 2/readme.md
@@ -0,0 +1,2 @@
+# Class 2 YT Link:
+https://www.youtube.com/watch?v=UZ8nqwGegz4&t=828s
